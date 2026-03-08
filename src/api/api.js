006d67cedba56bbdf7c60d3577ca27335f3a4010
Noor-Afsha <noorinkr1411@gmail.com>
2026-03-09 00:23:59 +0530

    added the deployed service live url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+  // baseURL: "http://localhost:8080/api"
+    baseURL: "https://chutney-backend-service-5.onrender.com/api"
+
 });
 
 export default api;
